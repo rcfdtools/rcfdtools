a6f4b507adb1144df60eadd91644ba8980e23b88
r.cfdtools <r.cfdtools@gmail.com>
2025-11-22 11:10:36 -0500

    General update.

--- tools/basicsum/main.py
+++ tools/basicsum/main.py
@@ -1,11 +1,9 @@
-import arrr
 from pyscript import document
 
-
 def translate_english(event):
-    input_text_a= document.querySelector("#a")
-    input_text_b= document.querySelector("#b")
-    a = input_text.a
-    b = input_text.b
+    input_a = document.querySelector("#a")
+    input_b = document.querySelector("#b")
+    a = input_a.value
+    b = input_b.value
     output_div = document.querySelector("#output")
     output_div.innerText = a+b
