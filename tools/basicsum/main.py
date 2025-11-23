3840815ee8fe48d9aa0ef57f84098c75e3c39d8e
r.cfdtools <r.cfdtools@gmail.com>
2025-11-23 10:09:20 -0500

    General update.

--- tools/basicsum/main.py
+++ tools/basicsum/main.py
@@ -5,7 +5,7 @@ def run(event):
     input_a = document.querySelector("#a")
     input_b = document.querySelector("#b")
     a = float(input_a.value)
-    b = floar(input_b.value)
+    b = float(input_b.value)
     c = a + b
     output_div = document.querySelector("#output")
     output_div.innerText = c
