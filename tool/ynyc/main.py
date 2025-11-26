c5c516f2a84227968850a172410c5e1e4e2ede23
r.cfdtools <r.cfdtools@gmail.com>
2025-11-26 17:38:18 -0500

    General update.

--- tool/ynyc/main.py
+++ tool/ynyc/main.py
@@ -15,7 +15,7 @@ def run(event):
     input_alpha = document.querySelector("#alpha")
     input_y2 = document.querySelector("#y2")
     input_y1 = document.querySelector("#y1")
-    input_iterat = document.querySelector("#iterat")
+    input_steps = document.querySelector("#steps")
 
     # read fields values
     UnitSys = input_UnitSys.value
@@ -30,7 +30,7 @@ def run(event):
     alpha = float(input_alpha.value)
     y1 = float(input_y1.value)
     y2 = float(input_y2.value)
-    iterat = float(input_iterat.value)
+    steps = float(input_steps.value)
     results = '\nResults:\n'
     area = area_trapezoid(b, z1, z2, y2)
     results += f'Area: {area}\n'
@@ -52,8 +52,8 @@ def run(event):
     if y2 < y1:
         y1 = y2
         y2 = Y1a
-    # eval # iterations > 0
-    if iterat <= 0: iterat = 32
+    # eval # step iterations > 0
+    if steps <= 0: steps = 32
     y1a = y1
     y2b = y2
 
