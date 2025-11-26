779a18317cc4df487f559e9e90f19a9f7fff1cd5
r.cfdtools <r.cfdtools@gmail.com>
2025-11-26 17:06:35 -0500

    General update.

--- tool/ynyc/main.py
+++ tool/ynyc/main.py
@@ -14,6 +14,7 @@ def run(event):
     input_n = document.querySelector("#n")
     input_alpha = document.querySelector("#alpha")
     input_y2 = document.querySelector("#y2")
+    input_y1 = document.querySelector("#y1")
     input_iterat = document.querySelector("#iterat")
 
     # read fields values
@@ -27,12 +28,35 @@ def run(event):
     n = float(input_n.value)
     alpha = float(input_alpha.value)
     alpha = float(input_alpha.value)
+    y1 = float(input_y1.value)
     y2 = float(input_y2.value)
+    iterat = float(input_iterat.value)
     results = '\nResults:\n'
     area = area_trapezoid(b, z1, z2, y2)
     results += f'Area: {area}\n'
     output_div = document.querySelector("#output")
     output_div.innerText = results
+
+    # pre validations
+    # units system eval
+    if UnitSys == 'SI':
+        c = 1
+        c_unit_text = "m"
+    else:
+        c = 1.486
+        c_unit_text = "ft"
+    # alpha validation
+    if alpha == 0: alpha = 1
+    # eval y2 > y1
+    y1a = y1
+    if y2 < y1:
+        y1 = y2
+        y2 = Y1a
+    # eval # iterations > 0
+    if iterat <= 0: iterat = 32
+    y1a = y1
+    y2b = y2
+
 
 def area_trapezoid(b, z1, z2, y):
     area = b * y + ((y ** 2) / 2) * (z1 + z2)
